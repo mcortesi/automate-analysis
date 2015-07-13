c085fa3ad30d20f23f3bf09daaa37fe415104541
Mariano A. Cortesi <mariano@zauberlabs.com>
2015-07-13 12:43:30 -0300

    setup webpack for assets

---
+++ assets/webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+
+  context: __dirname,
+  entry: {
+    'bundle': './src/index.js'
+  },
+
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "bundle.js",
+    publicPath: "/assets/"
+    //filename: "[name]/[hash].bundle.js"
+  },
+
+  resolve: {
+    extensions: ["", ".js"],
+  },
+
+  module: {
+    loaders: [
+      {
+      test: /\.jsx?$/,  exclude: /(node_modules|bower_components)/, loader: 'babel' },
+      { test: /\.less$/, loader: "style!css!less" },
+      { test: /\.(png|jpg|svg|gif|eot|woff|ttf)$/,
+        loader: 'file-loader?name=[path][name]-[hash].[ext]'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "root.jQuery": "jquery"
+    })
+  ],
+
+  ///////////////////////////////////////////////////////////////
+  // NOTE: COMMENT THIS for production!!
+  ///////////////////////////////////////////////////////////////
+  debug: true,
+  devtool: 'eval'
+  ///////////////////////////////////////////////////////////////
+};
+
+// check http://stackoverflow.com/questions/28969861/managing-jquery-plugin-dependency-in-webpack
+// for tips on legacy scripts
